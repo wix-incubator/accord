3c49534deecff26fcb5f28c8ffa7d4df5b0194d3
Tomer Gabel <tomerga@wix.com>
2013-11-13 18:56:30 +0200

    Added license information to source files

--- src/test/scala/com/tomergabel/accord/PrimitiveValidationTests.scala
+++ src/test/scala/com/tomergabel/accord/PrimitiveValidationTests.scala
@@ -1,11 +1,25 @@
+/*
+  Copyright 2013 Tomer Gabel
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+
+
 package com.tomergabel.accord
 
 import org.scalatest.{Matchers, WordSpec}
 import org.scalatest.matchers._
 
-/**
-* Created by tomer on 8/7/13.
-*/
 
 object PrimitiveValidationTests {
   import builder._
