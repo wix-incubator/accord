e85d54ee0d30673e243dcafb6778ea172bbe2f45
Tomer Gabel <tomerga@wix.com>
2013-11-14 11:28:58 +0200

    Added Implicits for alternative invocation syntax, some cleanup around the test code

--- src/test/scala/com/tomergabel/accord/PrimitiveValidationTests.scala
+++ src/test/scala/com/tomergabel/accord/PrimitiveValidationTests.scala
@@ -14,34 +14,12 @@
   limitations under the License.
  */
 
-
 package com.tomergabel.accord
 
 import org.scalatest.{Matchers, WordSpec}
-import org.scalatest.matchers._
+import PrimitiveSchema._
 
-
-object PrimitiveValidationTests {
-  import dsl._
-
-  case class Person( firstName: String, lastName: String )
-  case class Classroom( teacher: Person, students: Seq[ Person ] )
-
-  implicit val personValidator = validator[ Person ] { p =>
-    p.firstName is notEmpty
-    p.lastName is notEmpty
-  }
-
-  implicit val classValidator = validator[ Classroom ] { c =>
-    c.teacher is valid
-    c.students.each is valid
-    c.students have size > 0
-  }
-}
-
-import PrimitiveValidationTests._
-class PrimitiveValidationTests extends WordSpec with Matchers {
-
+class PrimitiveValidationTests extends WordSpec with Matchers with ResultMatchers {
   val personWithNoName = Person( "", "" )
   val personWithNoFirstName = Person( "", "last" )
   val personWithNoLastName = Person( "first", "" )
@@ -51,33 +29,6 @@ class PrimitiveValidationTests extends WordSpec with Matchers {
   val classWithInvalidTeacher = Classroom( personWithNoName, Seq( legitPerson1, legitPerson2, legitPerson3 ) )
   val classWithNoStudents = Classroom( legitPerson1, Seq.empty )
   val classWithInvalidStudent = Classroom( legitPerson1, Seq( legitPerson2, personWithNoLastName ) )
-
-  def failWith( expectedViolations: String* ) = new Matcher[ Result ] {
-    def apply( left: Result ): MatchResult =
-      left match {
-        case Success => MatchResult( matches = false, "Validation was successful", "Validation was not successful" )
-        case Failure( vlist ) =>
-          val violations = vlist.map { _.constraint }.toSet
-          val remainder = expectedViolations.toSet -- violations
-          val unexpected = violations.toSet -- expectedViolations.toSet
-          MatchResult( matches = remainder.isEmpty && unexpected.isEmpty,
-            "Validation failed with unexpected violations!\nExpected violations that weren't found:\n"
-              + remainder.mkString( "\t", "\n\t", "\n" )
-              + "Unexpected violations:\n" + unexpected.mkString( "\t", "\n\t", "\n" ),
-            // How to negate?
-            "Validation failed with unexpected violations!\nExpected violations that weren't found:\n"
-              + remainder.mkString( "\t", "\n\t", "\n" )
-              + "Unexpected violations:\n" + unexpected.mkString( "\t", "\n\t", "\n" )
-          )
-      }
-  }
-
-  val aFailure = new BeMatcher[ Result ] {
-    def apply( left: Result ) = MatchResult( left.isInstanceOf[ Failure ], "not a failure", "is a failure" )
-  }
-  val aSuccess = new BeMatcher[ Result ] {
-    def apply( left: Result ) = MatchResult( left == Success, "not a success", "is a success" )
-  }
 
   "personValidator" should {
     "fail a person with no first name" in {
---
+++ src/test/scala/com/tomergabel/accord/ResultMatchers.scala
@@ -0,0 +1,60 @@
+/*
+  Copyright 2013 Tomer Gabel
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+
+package com.tomergabel.accord
+
+import org.scalatest.Suite
+import org.scalatest.matchers.{BeMatcher, MatchResult, Matcher}
+
+/** Extends a test suite with a set of matchers over validation [[com.tomergabel.accord.Result]]s. */
+trait ResultMatchers {
+  self: Suite =>
+
+  /** Enables syntax like `someResult should failWith( "violation1", "violation2", ... )`.
+    * Unspecified violations will fail the test with an "unexpected violations" message. Specified
+    * violations that did not occur will fail the tests with an "expected violations weren't found"
+    * message.
+    */
+  def failWith( expectedViolations: String* ) = new Matcher[ Result ] {
+    def apply( left: Result ): MatchResult =
+      left match {
+        case Success => MatchResult( matches = false, "Validation was successful", "Validation was not successful" )
+        case Failure( vlist ) =>
+          val violations = vlist.map { _.constraint }.toSet
+          val remainder = expectedViolations.toSet -- violations
+          val unexpected = violations.toSet -- expectedViolations.toSet
+          MatchResult( matches = remainder.isEmpty && unexpected.isEmpty,
+            "Validation failed with unexpected violations!\nExpected violations that weren't found:\n"
+              + remainder.mkString( "\t", "\n\t", "\n" )
+              + "Unexpected violations:\n" + unexpected.mkString( "\t", "\n\t", "\n" ),
+            // How to negate?
+            "Validation failed with unexpected violations!\nExpected violations that weren't found:\n"
+              + remainder.mkString( "\t", "\n\t", "\n" )
+              + "Unexpected violations:\n" + unexpected.mkString( "\t", "\n\t", "\n" )
+          )
+      }
+  }
+
+  /** Enables syntax like `someResult should be( aFailure )` */
+  val aFailure = new BeMatcher[ Result ] {
+    def apply( left: Result ) = MatchResult( left.isInstanceOf[ Failure ], "not a failure", "is a failure" )
+  }
+
+  /** Enables syntax like `someResult should be( aSuccess )` */
+  val aSuccess = new BeMatcher[ Result ] {
+    def apply( left: Result ) = MatchResult( left == Success, "not a success", "is a success" )
+  }
+}
