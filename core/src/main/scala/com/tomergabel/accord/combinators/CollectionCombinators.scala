16f6f333750ee4702881ef0b99f5eb65084b8aae
Tomer Gabel <tomerga@wix.com>
2013-11-19 17:29:24 +0200

    Silly ScalaDoc style cleanup

--- core/src/main/scala/com/tomergabel/accord/combinators/CollectionCombinators.scala
+++ core/src/main/scala/com/tomergabel/accord/combinators/CollectionCombinators.scala
@@ -27,23 +27,23 @@ trait CollectionCombinators {
   type HasEmpty = { def isEmpty: Boolean }
 
   /**
-   * An implicit conversion to enable any collection-like object (e.g. strings, options) to be handled by the
-   * [[com.tomergabel.accord.combinators.Empty]] and [[com.tomergabel.accord.combinators..NotEmpty]]
-   * combinators.
-   *
-   * [[java.lang.String]] does not directly implement `isEmpty` (in practice it is implemented in
-   * [[scala.collection.IndexedSeqOptimized]], via an implicit conversion and an inheritance stack), and this is
-   * a case where the Scala compiler does not always infer structural types correctly. By requiring
-   * a view bound from `T` to [[scala.collection.GenTraversableOnce]] we can force any collection-like structure
-   * to conform to the structural type [[com.tomergabel.accord.combinators.HasEmpty]], and by requiring
-   * a view bound from `T` to [[com.tomergabel.accord.combinators.HasEmpty]] at the call site (e.g.
-   * [[com.tomergabel.accord.dsl.empty]]) we additionally support any class that directly conforms to the
-   * structural type as well.
-   *
-   * @param gto An object that is, or is implicitly convertible to, [[scala.collection.GenTraversableOnce]].
-   * @tparam T The type that conforms, directly or implicitly, to [[com.tomergabel.accord.combinators.HasEmpty]].
-   * @return The specified object, strictly-typed as [[com.tomergabel.accord.combinators.HasEmpty]].
-   */
+    * An implicit conversion to enable any collection-like object (e.g. strings, options) to be handled by the
+    * [[com.tomergabel.accord.combinators.Empty]] and [[com.tomergabel.accord.combinators..NotEmpty]]
+    * combinators.
+    *
+    * [[java.lang.String]] does not directly implement `isEmpty` (in practice it is implemented in
+    * [[scala.collection.IndexedSeqOptimized]], via an implicit conversion and an inheritance stack), and this is
+    * a case where the Scala compiler does not always infer structural types correctly. By requiring
+    * a view bound from `T` to [[scala.collection.GenTraversableOnce]] we can force any collection-like structure
+    * to conform to the structural type [[com.tomergabel.accord.combinators.HasEmpty]], and by requiring
+    * a view bound from `T` to [[com.tomergabel.accord.combinators.HasEmpty]] at the call site (e.g.
+    * [[com.tomergabel.accord.dsl.empty]]) we additionally support any class that directly conforms to the
+    * structural type as well.
+    *
+    * @param gto An object that is, or is implicitly convertible to, [[scala.collection.GenTraversableOnce]].
+    * @tparam T The type that conforms, directly or implicitly, to [[com.tomergabel.accord.combinators.HasEmpty]].
+    * @return The specified object, strictly-typed as [[com.tomergabel.accord.combinators.HasEmpty]].
+    */
   implicit def genericTraversableOnce2HasEmpty[ T <% scala.collection.GenTraversableOnce[_] ]( gto: T ): HasEmpty = gto
 
   /** A validator that operates on objects that can be empty, and succeeds only if the provided instance is
@@ -68,22 +68,22 @@ trait CollectionCombinators {
   type HasSize = { def size: Int }
 
   /**
-   * An implicit conversion to enable any collection-like object (e.g. strings, options) to be handled by the
-   * [[com.tomergabel.accord.combinators.Size]] combinator.
-   *
-   * [[java.lang.String]] does not directly implement `size` (in practice it is implemented in
-   * [[scala.collection.IndexedSeqOptimized]], via an implicit conversion and an inheritance stack), and this is
-   * a case where the Scala compiler does not always infer structural types correctly. By requiring
-   * a view bound from `T` to [[scala.collection.GenTraversableOnce]] we can force any collection-like structure
-   * to conform to the structural type [[com.tomergabel.accord.combinators.HasSize]], and by requiring
-   * a view bound from `T` to [[com.tomergabel.accord.combinators.HasSize]] at the call site (i.e.
-   * [[com.tomergabel.accord.dsl.size]]) we additionally support any class that directly conforms to the
-   * structural type as well.
-   *
-   * @param gto An object that is, or is implicitly convertible to, [[scala.collection.GenTraversableOnce]].
-   * @tparam T The type that conforms, directly or implicitly, to [[com.tomergabel.accord.combinators.HasSize]].
-   * @return The specified object, strictly-typed as [[com.tomergabel.accord.combinators.HasSize]].
-   */
+    * An implicit conversion to enable any collection-like object (e.g. strings, options) to be handled by the
+    * [[com.tomergabel.accord.combinators.Size]] combinator.
+    *
+    * [[java.lang.String]] does not directly implement `size` (in practice it is implemented in
+    * [[scala.collection.IndexedSeqOptimized]], via an implicit conversion and an inheritance stack), and this is
+    * a case where the Scala compiler does not always infer structural types correctly. By requiring
+    * a view bound from `T` to [[scala.collection.GenTraversableOnce]] we can force any collection-like structure
+    * to conform to the structural type [[com.tomergabel.accord.combinators.HasSize]], and by requiring
+    * a view bound from `T` to [[com.tomergabel.accord.combinators.HasSize]] at the call site (i.e.
+    * [[com.tomergabel.accord.dsl.size]]) we additionally support any class that directly conforms to the
+    * structural type as well.
+    *
+    * @param gto An object that is, or is implicitly convertible to, [[scala.collection.GenTraversableOnce]].
+    * @tparam T The type that conforms, directly or implicitly, to [[com.tomergabel.accord.combinators.HasSize]].
+    * @return The specified object, strictly-typed as [[com.tomergabel.accord.combinators.HasSize]].
+    */
   implicit def genericTraversableOnce2HasSize[ T <% scala.collection.GenTraversableOnce[_] ]( gto: T ): HasSize = gto
 
   /** A wrapper that operates on objects that provide a size, and provides validators based on te size of the
