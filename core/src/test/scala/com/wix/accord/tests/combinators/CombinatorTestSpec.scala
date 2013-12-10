fb593ae18a866483124c7a14c56a929d6bceb5a6
Tomer Gabel <tomerga@wix.com>
2013-12-10 21:17:39 +0200

    Changed copyright notice to reflect the project's origins at Wix

--- core/src/test/scala/com/wix/accord/tests/combinators/CombinatorTestSpec.scala
+++ core/src/test/scala/com/wix/accord/tests/combinators/CombinatorTestSpec.scala
@@ -1,5 +1,5 @@
 /*
-  Copyright 2013 Tomer Gabel
+  Copyright 2013 Wix.com
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
