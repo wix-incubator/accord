66e1dafbc82869891f1ee20aee395caa057f6a25
Tomer Gabel <tomerga@wix.com>
2014-01-26 22:32:19 +0200

    Fixed and cleaned up String-oriented DSL operations

--- core/src/main/scala/com/wix/accord/dsl/StringOps.scala
+++ core/src/main/scala/com/wix/accord/dsl/StringOps.scala
@@ -20,38 +20,51 @@ import com.wix.accord.Validator
 import com.wix.accord.combinators.StartsWith
 import com.wix.accord.combinators.EndsWith
 import com.wix.accord.combinators.MatchesRegex
+import scala.util.matching.Regex
+import java.util.regex.Pattern
 
 trait StringOps {
   /** Specifies a validator that operates on strings and succeeds only if the validation expression starts with
     * the specified prefix.
     */
-  def startsWith( prefix: String ): Validator[ String ] = new StartsWith( prefix )
+  def startWith( prefix: String ): Validator[ String ] = new StartsWith( prefix )
 
   /** Specifies a validator that operates on strings and succeeds only if the validation expression ends with
     * the specified suffix.
     */
-  def endsWith( suffix: String ): Validator[ String ] = new EndsWith( suffix )
+  def endWith( suffix: String ): Validator[ String ] = new EndsWith( suffix )
 
   /** Specifies a validator that operates on strings and succeeds only if the validation expression matches the
     * specified regular expression.
     */
-  def matches( regex: String ): Validator[ String ] = matches( regex.r.pattern )
+  def matchRegex( regex: String ): Validator[ String ] = matchRegex( regex.r )
+
 
   /** Specifies a validator that operates on strings and succeeds only if the validation expression matches the
     * specified regular expression.
     */
-  def matches( pattern: java.util.regex.Pattern ): Validator[ String ] = new MatchesRegex( pattern, partialMatchAllowed = true )
+  def matchRegex( regex: Regex ): Validator[ String ] = matchRegex( regex.pattern )
+
+  /** Specifies a validator that operates on strings and succeeds only if the validation expression matches the
+    * specified regular expression.
+    */
+  def matchRegex( pattern: Pattern ): Validator[ String ] = new MatchesRegex( pattern, partialMatchAllowed = true )
 
   /** Specifies a validator that operates on strings and succeeds only if the validation expression **fully**
     * matches the specified regular expression. See [[com.wix.accord.combinators.StringCombinators.MatchesRegex]]
     * for a full explanation of the difference between partial and full matching.
     */
-  def fullyMatches( pattern: java.util.regex.Pattern ): Validator[ String ] = new MatchesRegex( pattern, partialMatchAllowed = false )
+  def matchRegexFully( regex: String ): Validator[ String ] = matchRegexFully( regex.r )
 
   /** Specifies a validator that operates on strings and succeeds only if the validation expression **fully**
     * matches the specified regular expression. See [[com.wix.accord.combinators.StringCombinators.MatchesRegex]]
     * for a full explanation of the difference between partial and full matching.
     */
-  def fullyMatches( regex: String ): Validator[ String ] = fullyMatches( regex.r.pattern )
+  def matchRegexFully( regex: Regex ): Validator[ String ] = matchRegexFully( regex.pattern )
 
+  /** Specifies a validator that operates on strings and succeeds only if the validation expression **fully**
+    * matches the specified regular expression. See [[com.wix.accord.combinators.StringCombinators.MatchesRegex]]
+    * for a full explanation of the difference between partial and full matching.
+    */
+  def matchRegexFully( pattern: Pattern ): Validator[ String ] = new MatchesRegex( pattern, partialMatchAllowed = false )
 }
---
+++ core/src/test/scala/com/wix/accord/tests/dsl/StringOpsTests.scala
@@ -0,0 +1,105 @@
+/*
+  Copyright 2013 Wix.com
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+
+package com.wix.accord.tests.dsl
+
+import com.wix.accord.scalatest.ResultMatchers
+import org.scalatest.{WordSpec, Matchers, FlatSpec}
+import com.wix.accord.Validator
+
+class StringOpsTests extends WordSpec with Matchers with ResultMatchers {
+  import StringOpsTests._
+
+  // Quick and dirty helper to make the tests a little more terse
+  implicit class EnrichString( text: String ) {
+    def validatedWith( validator: Validator[ Test ] ) = validator apply Test( text )
+  }
+
+  "startWith" should {
+    "successfully validate a correct string" in {
+      "testing" validatedWith startsWithValidator should be( aSuccess )
+    }
+    "successfully validate an incorrect string" in {
+      "some string" validatedWith startsWithValidator should be( aFailure )
+    }
+  }
+
+  "endWith" should {
+    "successfully validate a correct string" in {
+      "some test" validatedWith endsWithValidator should be( aSuccess )
+    }
+    "successfully validate an incorrect string" in {
+      "some string" validatedWith endsWithValidator should be( aFailure )
+    }
+  }
+  
+  "matchRegex" should {
+    "successfully validate a correct string based on a text pattern" in {
+      "tests galore" validatedWith matchRegexValidator should be( aSuccess )
+    }
+    "successfully validate an incorrect string based on a text pattern" in {
+      "some string" validatedWith matchRegexValidator should be( aFailure )
+    }
+    "successfully validate a correct string based on a Scala regex" in {
+      "tests galore" validatedWith matchRegexByScalaValidator should be( aSuccess )
+    }
+    "successfully validate an incorrect string based on a Scala regex" in {
+      "some string" validatedWith matchRegexByScalaValidator should be( aFailure )
+    }
+    "successfully validate a correct string based on a java.util.regex.Pattern" in {
+      "tests galore" validatedWith matchRegexByPatternValidator should be( aSuccess )
+    }
+    "successfully validate an incorrect string based on a java.util.regex.Pattern" in {
+      "some string" validatedWith matchRegexByPatternValidator should be( aFailure )
+    }
+  }
+
+  "matchRegexFully" should {
+    "successfully validate a correct string based on a text pattern" in {
+      "so many tests" validatedWith matchRegexFullyValidator should be( aSuccess )
+    }
+    "successfully validate an incorrect string based on a text pattern" in {
+      "tests galore" validatedWith matchRegexFullyValidator should be( aFailure )
+    }
+    "successfully validate a correct string based on a Scala regex" in {
+      "so many tests" validatedWith matchRegexFullyByScalaValidator should be( aSuccess )
+    }
+    "successfully validate an incorrect string based on a Scala regex" in {
+      "tests galore" validatedWith matchRegexFullyByScalaValidator should be( aFailure )
+    }
+    "successfully validate a correct string based on a java.util.regex.Pattern" in {
+      "so many tests" validatedWith matchRegexFullyByPatternValidator should be( aSuccess )
+    }
+    "successfully validate an incorrect string based on a java.util.regex.Pattern" in {
+      "tests galore" validatedWith matchRegexFullyByPatternValidator should be( aFailure )
+    }
+  }
+}
+
+object StringOpsTests {
+  import com.wix.accord.dsl._
+  case class Test( s: String )
+
+  val               startsWithValidator = validator[ Test ] { _.s should startWith( "test" ) }
+  val                 endsWithValidator = validator[ Test ] { _.s should endWith( "test" ) }
+  val               matchRegexValidator = validator[ Test ] { _.s should matchRegex( "test(s?)" ) }
+  val        matchRegexByScalaValidator = validator[ Test ] { _.s should matchRegex( "test(s?)".r ) }
+  val      matchRegexByPatternValidator = validator[ Test ] { _.s should matchRegex( "test(s?)".r.pattern ) }
+  val          matchRegexFullyValidator = validator[ Test ] { _.s should matchRegexFully( ".*test(s?)" ) }
+  val   matchRegexFullyByScalaValidator = validator[ Test ] { _.s should matchRegexFully( ".*test(s?)".r ) }
+  val matchRegexFullyByPatternValidator = validator[ Test ] { _.s should matchRegexFully( ".*test(s?)".r.pattern ) }
+}
+
