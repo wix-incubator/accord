220f95ed0959ab37538113ba6194712c2c2efdaa
Tomer Gabel <tomerga@wix.com>
2014-12-24 09:28:18 +0200

    Example is taking shape.

--- examples/src/main/scala/com/wix/accord/examples/Domain.scala
+++ examples/src/main/scala/com/wix/accord/examples/Domain.scala
@@ -3,53 +3,15 @@ package com.wix.accord.examples
 /**
  * Created by tomer on 12/24/14.
  */
-trait BaseDomain {
-  import com.wix.accord._
-  import dsl._
 
-  trait Person {
-    def name: String
-    def surname: String
-    def age: Int
-  }
-  object Person {
-    implicit val personValidator: Validator[ Person ] =
-      validator[ Person ] { person =>
-        person.name is notEmpty
-        person.surname is notEmpty
-        person.age is between( 0, 120 )
-      }
-  }
+trait Person {
+  def name: String
+  def surname: String
+  def age: Int
 }
 
-object Domain extends BaseDomain {
+case class Adult( name: String, surname: String, age: Int, contactInfo: String ) extends Person
 
-  case class Minor( name: String, surname: String, age: Int, guardians: Set[ Adult ] ) extends Person
-  case class Adult( name: String, surname: String, age: Int, contactInfo: String ) extends Person
-  case class Classroom( grade: Int, teacher: Adult, students: Set[ Minor ] )
+case class Minor( name: String, surname: String, age: Int, guardians: Set[ Adult ] ) extends Person
 
-  import com.wix.accord._
-  import dsl._
-
-  implicit val adultValidator: Validator[ Adult ] =
-    validator[ Adult ] { adult =>
-      adult is valid[ Person ]
-      adult.age should be >= 18
-      adult.contactInfo is notEmpty
-    }
-
-  implicit val minorValidator: Validator[ Minor ] =
-    validator[ Minor ] { minor =>
-      minor is valid[ Person ]
-      minor.age should be < 18
-      minor.guardians is notEmpty
-      minor.guardians.each is valid( adultValidator )
-    }
-
-  implicit val classroomValidator: Validator[ Classroom ] =
-    validator[ Classroom ] { classroom =>
-      classroom.grade is between( 1, 12 )
-      classroom.teacher is valid
-      classroom.students has size.between( 18, 25 )
-    }
-}
+case class Classroom( grade: Int, teacher: Adult, students: Set[ Minor ] )
