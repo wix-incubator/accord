5be2b8182a88ae54004458a919d39fd1b68f51bb
Tomer Gabel <tomerga@wix.com>
2015-05-09 23:45:54 +0300

    Added prototype test for nested validation errors

--- spring3/src/test/scala/com/wix/accord/spring/AccordValidatorAdapterTest.scala
+++ spring3/src/test/scala/com/wix/accord/spring/AccordValidatorAdapterTest.scala
@@ -18,7 +18,7 @@ package com.wix.accord.spring
 
 import com.wix.accord.dsl
 import org.scalatest.{WordSpec, Matchers}
-import org.springframework.validation.{BeanPropertyBindingResult, Errors}
+import org.springframework.validation.{FieldError, BeanPropertyBindingResult, Errors}
 import scala.collection.JavaConversions._
 
 import AccordValidatorAdapterTest._
@@ -59,16 +59,43 @@ class AccordValidatorAdapterTest extends WordSpec with Matchers {
       val returnedMessages = errors.getAllErrors map { _.getDefaultMessage }
       returnedMessages should contain only( expectedErrorMessages:_* )
     }
+
+    "correctly render nested validation errors" in {
+      val contained = Contained( "not good!" )
+      val container = Container( "ok", contained )
+
+      val errors: Errors = new BeanPropertyBindingResult( container, "container" )
+      val adapter = new AccordValidatorAdapter( containerValidator )
+      adapter.validate( container, errors )
+      errors.getAllErrors should have size 1
+      errors.getAllErrors.head shouldBe a[ FieldError ]
+
+      val fe = errors.getAllErrors.head.asInstanceOf[ FieldError ]
+      fe.getField shouldEqual "contained"
+      fe.getRejectedValue shouldEqual Contained( "not good!" )
+      // What should actually go in here?
+    }
   }
 
 }
 
 private object AccordValidatorAdapterTest {
-  case class TestClass( f1: String, f2: Int )
-
   import dsl._
+
+  case class TestClass( f1: String, f2: Int )
   implicit val testClassValidator = validator[ TestClass ] { ae =>
     ae.f1 has size <= 10
     ae.f2 should be <= 10
+  }
+
+  case class Contained( field: String )
+  implicit val containedValidator = validator[ Contained ] { c =>
+    c.field has ( size >= 2 and size <= 4 )
+  }
+
+  case class Container( field: String, contained: Contained )
+  implicit val containerValidator = validator[ Container ] { c =>
+    c.field is notEmpty
+    c.contained is valid
   }
 }
