f8d54464c758dc319c91d64ec532285c6fa3a558
Tomer Gabel <tomer@tomergabel.com>
2015-07-19 11:04:21 +0300

    Tightened down Violation base trait to support exhaustiveness checks in client code (closes #45)

--- api/src/main/scala/com/wix/accord/Result.scala
+++ api/src/main/scala/com/wix/accord/Result.scala
@@ -17,7 +17,7 @@
 package com.wix.accord
 
 /** A base trait for all violation types. */ 
-trait Violation {
+sealed trait Violation {
   /** The actual runtime value of the object under validation. */
   def value: Any
   /** A textual description of the constraint being violated (for example, "must not be empty"). */
