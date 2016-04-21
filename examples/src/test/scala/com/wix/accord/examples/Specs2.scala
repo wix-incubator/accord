6ec421aa28a3929d8c3f17962403fd15b4b1cef6
Tomer Gabel <tomer@tomergabel.com>
2016-04-21 18:03:54 +0300

    Additional minor cleanup

--- examples/src/test/scala/com/wix/accord/examples/Specs2.scala
+++ examples/src/test/scala/com/wix/accord/examples/Specs2.scala
@@ -1,3 +1,19 @@
+/*
+  Copyright 2013-2015 Wix.com
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+
 package com.wix.accord.examples
 
 import com.wix.accord.specs2.ResultMatchers
@@ -6,9 +22,6 @@ import org.specs2.mutable.Specification
 
 import scala.util.Random
 
-/**
- * Created by tomer on 12/24/14.
- */
 class Specs2 extends Specification with Matchers with ResultMatchers {
   import com.wix.accord._
 
