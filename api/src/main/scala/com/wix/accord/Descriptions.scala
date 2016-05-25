903ba8ee344f4e916ec38f0c176518d933928968
Tomer Gabel <tomer@tomergabel.com>
2016-05-25 17:25:01 +0300

    Some docs

--- api/src/main/scala/com/wix/accord/Descriptions.scala
+++ api/src/main/scala/com/wix/accord/Descriptions.scala
@@ -1,17 +1,47 @@
+/*
+  Copyright 2013-2015 Wix.com
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+
 package com.wix.accord
 
-import com.sun.org.glassfish.gmbal.Description
-
-/**
-  * Created by tomerga on 05/05/2016.
-  */
 object Descriptions {
+  /** Root trait whose various cases describe a single Object Under Validation (OUV). */
   sealed trait Description
+  /**
+    * An empty (i.e. unknown) description. This is the default state of any violation prior to applying additional
+    * information via [[com.wix.accord.Descriptions.combine]].
+    */
   case object Empty extends Description
+  /** Denotes an index access (e.g. accessing the nth element of an array). */
   case class Indexed( index: Long, of: Description = Empty ) extends Description
+  /** Denotes an explicit textual description, typically provided via the DSL `as` keyword. */
   case class Explicit( description: String ) extends Description
+
+  /**
+    * Denotes a generic textual description, typically a piece of code, where a better description could
+    * not be generated.
+    */
   case class Generic( description: String ) extends Description
+
+  /**
+    * Denotes an indirection chain. For example, the expression `field.subfield.subsubfield` would result in
+    * a description like `AccessChain( "field", "subfield", "subsubfield" )`.
+    */
   case class AccessChain( elements: String* ) extends Description
+
+  /** Denotes a self-reference (i.e. using a single positional wildcard in a lambda). */
   case object SelfReference extends Description
 
   // Description algebra --
@@ -26,7 +56,7 @@ object Descriptions {
     case ( lhs, SelfReference ) => lhs
     case ( SelfReference, rhs ) => rhs
 
-    case ( AccessChain( left @ _* ), AccessChain( right @ _* ) ) => AccessChain( ( left ++ right ) :_* )
+    case ( AccessChain( left @ _* ), AccessChain( right @ _* ) ) => AccessChain( left ++ right :_* )
 
     case ( lhs, rhs ) =>
       throw new IllegalArgumentException( s"Cannot combine description '$lhs' with '$rhs'" )
