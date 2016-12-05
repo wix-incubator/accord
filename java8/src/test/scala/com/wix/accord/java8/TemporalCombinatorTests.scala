f2468d84840514c1d95c3435ad3c2e50d77c1c0b
Tomer Gabel <tomer@tomergabel.com>
2016-12-05 13:39:53 +0200

    [#90] Initial implementation of temporal combinators

--- java8/src/test/scala/com/wix/accord/java8/TemporalCombinatorTests.scala
+++ java8/src/test/scala/com/wix/accord/java8/TemporalCombinatorTests.scala
@@ -17,17 +17,11 @@
 package com.wix.accord.java8
 
 import java.time.Instant
-import java.time.temporal.{ChronoUnit, Temporal}
+import java.time.temporal.ChronoUnit
 
-import com.wix.accord.NullSafeValidator
 import com.wix.accord.scalatest.CombinatorTestSpec
 
 class TemporalCombinatorTests extends CombinatorTestSpec {
-
-  class Before[ T <: Temporal with Comparable[ T ] ]( right: T ) 
-    extends NullSafeValidator[ T ]( _ => ???, _ => ??? )
-  class After[ T <: Temporal with Comparable[ T ] ]( right: T )
-    extends NullSafeValidator[ T ]( _ => ???, _ => ??? )
 
   "Before combinator" should {
     "successfully validate a temporal that represents an instant before the specified temporal" in {
@@ -56,8 +50,8 @@ class TemporalCombinatorTests extends CombinatorTestSpec {
     "render a correct rule violation" in {
       val left = Instant.now()
       val right = left.plus( 1L, ChronoUnit.DAYS )
-      val validator = new Before( right )
-      validator( left ) should failWith( s"must be before ${ right.toString }" )
+      val validator = new After( right )
+      validator( left ) should failWith( s"must be after ${ right.toString }" )
     }
   }
 }
