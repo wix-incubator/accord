c91935d89b8b6e105409a91efcae9990c372f461
Tomer Gabel <tomer@tomergabel.com>
2016-12-08 10:40:10 +0200

    Updated copyright notice

--- spring3/src/test/scala/com/wix/accord/spring/AccordValidatorAdapterTest.scala
+++ spring3/src/test/scala/com/wix/accord/spring/AccordValidatorAdapterTest.scala
@@ -1,5 +1,5 @@
 /*
-  Copyright 2013-2015 Wix.com
+  Copyright 2013-2016 Wix.com
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
