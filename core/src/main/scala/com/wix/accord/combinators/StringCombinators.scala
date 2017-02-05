3765fd5378b436d80c21080459bb9affb2082641
Dai Akatsuka <d.akatsuka@gmail.com>
2017-02-06 00:26:33 +0900

    Fix constraint string

--- core/src/main/scala/com/wix/accord/combinators/StringCombinators.scala
+++ core/src/main/scala/com/wix/accord/combinators/StringCombinators.scala
@@ -33,11 +33,11 @@ trait StringCombinators {
 
   /** A validator that succeeds only if the provided string is not blank. */
   class NotBlank
-    extends NullSafeValidator[ String ]( !_.trim.isEmpty, _ -> "must not blank" )
+    extends NullSafeValidator[ String ]( !_.trim.isEmpty, _ -> "must not be blank" )
 
   /** A validator that succeeds only if the provided string is blank. */
   class Blank
-    extends NullSafeValidator[ String ]( _.trim.isEmpty, _ -> "must blank" )
+    extends NullSafeValidator[ String ]( _.trim.isEmpty, _ -> "must be blank" )
 
   /** A validator that succeeds only if the provided string matches the specified pattern.
     *
